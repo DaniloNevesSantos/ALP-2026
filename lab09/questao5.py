8168f8197509d1f2cf75f4ebbec84c8c8572a7c3
DaniloNevesSantos <danilopaulosantos9@gmail.com>
2026-06-03 15:44:44 -0300

    lab9

---
+++ lab09/questao5.py
@@ -0,0 +1,11 @@
+n = int(input("Quantidade de valores:  "))
+n2 = 0
+soma = 0
+cont = 0
+while n > cont:
+    cont += 1
+    n2 = int(input("número:  "))
+    soma += n2
+print(f'Soma é {soma}.')
+print(f'quantidade de {n} ')
+print(f'Média é {soma / n}')
